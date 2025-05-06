1700d3b29433eb4a97f04bbfb2c3499acbf00d24
Antonio Navarro <anavarro67@hotmail.com>
2025-05-06 15:32:48 +0200

    create register form using react-hook-form

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom"
 
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <>
+        <h1 className="text-4xl text-white font-bold"> Acceder a tu cuenta </h1>
+        <nav className="mt-10">
+        <Link to='/register'>
+          <p className="text-white text-center text-lg"> ¿No tienes cuenta? Crearla aquí</p>
+          
+        </Link>
+      </nav>
+
+    </>
   )
 }
 
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,16 +1,17 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-
-
+import RegisterPage from "./pages/RegisterPage";
+import AuthLayout from "./Layouts/AuthLayout";
 
 export default function Router() {
 
     return ([
         <BrowserRouter>
         <Routes>
-            <Route>                
+            <Route element={<AuthLayout />}>                
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
             </Route>
         </Routes>
         </BrowserRouter>
