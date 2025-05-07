2286bcb35520992ad747eecf1038da81dcffdde7
Antonio Navarro <anavarro67@hotmail.com>
2025-05-07 08:47:35 +0200

    add validations to form fields

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -4,15 +4,23 @@ import { useForm } from 'react-hook-form';
 
 const RegisterPage = () => {
   
-  const {register, watch, handleSubmit} = useForm()
+  const {
+    register, 
+    watch, 
+    handleSubmit, 
+    formState: { errors }} = useForm()
   
-  return (
+    const handleRegister = (data: any) => {
+        console.log(data)
+    }
+  
+return (
     <>
       
       <h1 className="text-4xl text-white font-bold"> Crear cuenta </h1>
 
-      <form 
-    onSubmit={() => {}}
+      <form onSubmit={handleSubmit(handleRegister)}
+    
     className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
 >
     <div className="grid grid-cols-1 space-y-3">
@@ -22,7 +30,13 @@ const RegisterPage = () => {
             type="text"
             placeholder="Tu Nombre"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register('name', { required: 'El nombre es obligatorio' })}
+
         />
+            {errors.name &&
+                <p className='bg-red-50 text-red-600 p-3 uppercase text-sm text-center '> { String(errors.name.message)} </p>
+            }
+
     </div>
     <div className="grid grid-cols-1 space-y-3">
         <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
@@ -31,7 +45,14 @@ const RegisterPage = () => {
             type="email"
             placeholder="Email de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register('email', { required: 'El email es obligatorio' })}
+
         />
+            {errors.email &&
+                <p className='bg-red-50 text-red-600 p-3 uppercase text-sm text-center '> { String(errors.email.message)} </p>
+            }
+
+
     </div>
     <div className="grid grid-cols-1 space-y-3">
         <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
@@ -40,7 +61,11 @@ const RegisterPage = () => {
             type="text"
             placeholder="Nombre de usuario: sin espacios"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register('handle', { required: 'El handle es obligatorio' })}
         />
+            {errors.handle &&
+                <p className='bg-red-50 text-red-600 p-3 uppercase text-sm text-center '> { String(errors.handle.message)} </p>
+            }
     </div>
     <div className="grid grid-cols-1 space-y-3">
         <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
@@ -49,7 +74,13 @@ const RegisterPage = () => {
             type="password"
             placeholder="Password de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register('password', { required: 'El password es obligatorio' })}
+
         />
+                    {errors.password &&
+                <p className='bg-red-50 text-red-600 p-3 uppercase text-sm text-center '> { String(errors.password.message)} </p>
+            }
+
     </div>
 
     <div className="grid grid-cols-1 space-y-3">
@@ -59,7 +90,12 @@ const RegisterPage = () => {
             type="password"
             placeholder="Repetir Password"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register('passwordConfirmation', { required: 'La confirmación de la password es obligatoria ' })}
         />
+            {errors.passwordConfirmation &&
+                <p className='bg-red-50 text-red-600 p-3 uppercase text-sm text-center '> { String(errors.passwordConfirmation.message)} </p>
+            }
+
     </div>
 
     <input
